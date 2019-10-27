c725bce4e04207819cb231c7973bdc5aa1ebd094
Ryan Baxley <ryan.baxley1@gmail.com>
2019-10-27 17:54:28 -0400

    Add newName function

--- src/creeps/index.ts
+++ src/creeps/index.ts
@@ -1,9 +1,17 @@
 import harvester from './harvester';
 
+function newName(role: Creep['memory']['role']) {
+  for (let i = 1; i < 100; i++) {
+    const name = `${role}_${i}`;
+    if (!Game.creeps[name]) return name;
+  }
+  return String(Math.random());
+}
+
 export function spawnCreeps() {
-  if (Object.values(Game.creeps).length > 0) return;
+  if (Object.values(Game.creeps).length > 10) return;
   Object.values(Game.spawns).forEach(spawn => {
-    spawn.spawnCreep(['work', 'carry', 'move'], 'test', {
+    spawn.spawnCreep(['work', 'carry', 'move'], newName('harvester'), {
       memory: { role: 'harvester' },
     });
   });
