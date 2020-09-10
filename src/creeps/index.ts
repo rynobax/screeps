9f347091894e48e371e483081314d2c018adb693
Ryan Baxley <ryan.baxley1@gmail.com>
2020-09-10 14:01:05 -0400

    Refactor spawning logic to be more flexible

--- src/creeps/index.ts
+++ src/creeps/index.ts
@@ -11,7 +11,7 @@ function newName(role: Role) {
 }
 
 const roleCount = (role: Role) =>
-  Object.values(Game.creeps).filter(c => c.memory.role === role).length;
+  Object.values(Game.creeps).filter((c) => c.memory.role === role).length;
 
 const spawnCreep = (
   spawn: StructureSpawn,
@@ -25,33 +25,50 @@ const spawnCreep = (
     } as any,
   });
 
-function desiredHarvesters(spawn: StructureSpawn) {
-  return spawn.room.find(FIND_SOURCES).length * 3;
+function desiredCount(role: Role, spawn: StructureSpawn) {
+  switch (role) {
+    case 'harvester':
+      return spawn.room.find(FIND_SOURCES).length * 3;
+    default:
+      return 0;
+  }
 }
+
+type RoleInfo = {
+  type: Role;
+  runTask: (creep: Creep) => void;
+  parts: BodyPartConstant[];
+};
+
+const creepTypes: Record<Role, RoleInfo> = {
+  harvester: {
+    type: 'harvester',
+    runTask: (creep: Creep) => doTask(creep, ['harvest', 'store', 'upgrade']),
+    parts: [WORK, CARRY, MOVE],
+  },
+};
 
 export function spawnCreeps() {
   const spawn = Object.values(Game.spawns)[0];
 
-  if (roleCount('harvester') < desiredHarvesters(spawn)) {
+  for (const { type, parts } of Object.values(creepTypes)) {
+    const have = roleCount(type);
+    const need = desiredCount(type, spawn);
+    if (have < need) {
+      console.log(`spawning ${type} (${have + 1}/${need})`)
+      spawnCreep(spawn, type, parts);
+    }
+  }
+
+  if (roleCount('harvester') < desiredCount('harvester', spawn)) {
     spawnCreep(spawn, 'harvester', [WORK, CARRY, MOVE]);
   }
 }
 
-const harvesterTasks = [
-  'harvest' as const,
-  'store' as const,
-  'upgrade' as const,
-];
-const harvester = (creep: Creep) => doTask(creep, harvesterTasks);
-
-const creepTypes = {
-  harvester,
-};
-
 export function commandCreeps() {
-  Object.values(Game.creeps).forEach(creep => {
+  Object.values(Game.creeps).forEach((creep) => {
     if (creep.spawning) return;
-    const run = creepTypes[creep.memory.role];
+    const run = creepTypes[creep.memory.role].runTask;
     run(creep as any);
   });
 }
